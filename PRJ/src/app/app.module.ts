d967237e639d7b37787c98e6c4f6ca385f52afaf
Fadi Barakat <fadibar@gmail.com>
2020-05-29 17:07:07 +0300

    Updated layout of the components

--- PRJ/src/app/app.module.ts
+++ PRJ/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { HeaderComponent } from './header/header.component';
     ShoppingListComponent,
     ShoppingEditComponent,
     HeaderComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule
