1385d4711d7ee83d809b32c0f705653b2463225b
Fadi Barakat <fadibar@gmail.com>
2020-06-12 21:26:32 +0300

    Added directive to open/close dropdown menus

--- PRJ/src/app/app.module.ts
+++ PRJ/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { RecipesComponent } from './recipes/recipes.component';
     ShoppingEditComponent,
     HeaderComponent,
     RecipesComponent,
+    DropdownDirective,
   ],
   imports: [
     BrowserModule
