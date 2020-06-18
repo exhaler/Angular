eae2f9a71410175de32d598b55293a7c392ea8f6
Fadi Barakat <fadibar@gmail.com>
2020-06-18 19:15:29 +0300

    Add recipes to shopping list

--- PRJ/src/app/recipes/recipe.service.ts
+++ PRJ/src/app/recipes/recipe.service.ts
@@ -1,7 +1,10 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -9,11 +12,21 @@ export class RecipeService {
     new Recipe(
       'Name test',
       'Desc test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Fries', 20),
+      ]
     ),
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
