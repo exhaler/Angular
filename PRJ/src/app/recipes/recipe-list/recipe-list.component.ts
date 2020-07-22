a9c8b126394cfff50def88b0e7e74b2c9cfc69cc
Fadi Barakat <fadibar@gmail.com>
2020-07-22 11:53:06 +0300

    Deleting an ingredient

--- PRJ/src/app/recipes/recipe-list/recipe-list.component.ts
+++ PRJ/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+
+import { Subscription } from 'rxjs';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -9,8 +11,9 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(
     private RecipeService: RecipeService,
@@ -19,7 +22,7 @@ export class RecipeListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.RecipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+    this.subscription = this.RecipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
       this.recipes = recipes;
     })
     this.recipes = this.RecipeService.getRecipes();
@@ -27,5 +30,9 @@ export class RecipeListComponent implements OnInit {
 
   onNewRecipe() {
     this.router.navigate(['new'], { relativeTo: this.route });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
