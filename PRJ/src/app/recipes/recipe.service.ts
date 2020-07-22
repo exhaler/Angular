eaa3cdfc8742df44cb83cee274e9d59322328ed0
Fadi Barakat <fadibar@gmail.com>
2020-07-22 11:33:58 +0300

    Added detele and cancel functions

--- PRJ/src/app/recipes/recipe.service.ts
+++ PRJ/src/app/recipes/recipe.service.ts
@@ -15,19 +15,13 @@ export class RecipeService {
       'Name test 1',
       'Desc test 1',
       'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('Fries', 20),
-      ]
+      [new Ingredient('Meat', 1), new Ingredient('Fries', 20)]
     ),
     new Recipe(
       'Name test 2',
       'Desc test 2',
       'https://cdn.pixabay.com/photo/2017/06/28/17/40/chicken-2451482_960_720.jpg',
-      [
-        new Ingredient('Chicken', 2),
-        new Ingredient('Salad', 40),
-      ]
+      [new Ingredient('Chicken', 2), new Ingredient('Salad', 40)]
     ),
   ];
 
@@ -51,7 +45,12 @@ export class RecipeService {
   }
 
   updateRecipe(index: number, newRecipe: Recipe) {
-    this.recipes[index] = newRecipe
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
     this.recipesChanged.next(this.recipes.slice());
   }
 }
